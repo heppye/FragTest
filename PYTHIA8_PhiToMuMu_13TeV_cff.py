452324758239702f60c8c5265df4ea45af29ff43
Ye Chen <ye.chen@cern.ch>
2015-01-11 20:16:47 +0100

    Update PYTHIA8_PhiToMuMu_13TeV_cff.py

--- PYTHIA8_PhiToMuMu_13TeV_cff.py
+++ PYTHIA8_PhiToMuMu_13TeV_cff.py
@@ -1,3 +1,4 @@
+# For BPH Trigger Study Only
 import FWCore.ParameterSet.Config as cms
 from Configuration.Generator.Pythia8CommonSettings_cfi import *
 from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
